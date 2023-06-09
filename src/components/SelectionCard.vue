0673c90d26b515e4cbaf321ff73aece6230b8286
rifaldikn <rifaldikn@gmail.com>
2023-06-09 16:19:17 +0700

    add payment page

---
+++ src/components/SelectionCard.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineEmits(["click"]);
+
+defineProps({
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  activeColor: {
+    type: String,
+    default: "success",
+  },
+  baseColor: {
+    type: String,
+    default: "grey",
+  },
+  height: {
+    type: [String, Number],
+    default: "60px",
+  },
+});
+</script>
+
+<template>
+  <v-card
+    width="180"
+    :min-height="height"
+    variant="outlined"
+    :color="isActive ? activeColor : baseColor"
+    :class="{ 'bg-success-10': isActive }"
+    class="pa-2"
+    @click="$emit('click')"
+    hover
+  >
+    <slot name="content">
+      <v-row class="align-center fill-height" no-gutters>
+        <v-col cols="10" class="my-auto" :class="{ 'text-black': isActive }">
+          <slot> </slot>
+        </v-col>
+
+        <v-col cols="2">
+          <v-icon v-if="isActive" :color="activeColor" size="small">
+            mdi-check
+          </v-icon>
+        </v-col>
+      </v-row>
+    </slot>
+  </v-card>
+</template>
--- src/views/payment/components/PaymentStep.vue
+++ src/views/payment/components/PaymentStep.vue
@@ -1,5 +1,76 @@
-<script setup></script>
+<script setup>
+import { usePaymentStore } from "../Payment.store";
+import { toCurrency } from "@/helpers/filters";
+
+import CardTitle from "@/components/CardTitle.vue";
+import SelectionCard from "@/components/SelectionCard.vue";
+
+const store = usePaymentStore();
+</script>
 
 <template>
-  <v-row></v-row>
+  <v-row>
+    <v-col cols="12">
+      <CardTitle title="Shipment" />
+    </v-col>
+    <v-col
+      cols="auto"
+      v-for="(shipment, key) in store.shipmentMethods"
+      :key="key"
+    >
+      <SelectionCard
+        :is-active="store.shipment == key"
+        @click="store.shipment = key"
+      >
+        <div class="text-caption font-weight-bold">
+          {{ key }}
+        </div>
+        <div class="text-subtitle-1 font-weight-bold">
+          {{
+            toCurrency({
+              value: shipment.price,
+              locales: "en-US",
+              style: "decimal",
+            })
+          }}
+        </div>
+      </SelectionCard>
+    </v-col>
+  </v-row>
+
+  <v-row>
+    <v-col cols="12">
+      <CardTitle title="Payment" />
+    </v-col>
+    <v-col cols="auto" v-for="payment in store.paymentMethods" :key="payment">
+      <SelectionCard
+        height="100%"
+        :is-active="store.payment == payment"
+        @click="store.payment = payment"
+      >
+        <div
+          class="font-weight-bold"
+          :class="{
+            'text-subtitle-1 font-weight-bold': payment != 'e-Wallet',
+            'text-caption': payment == 'e-Wallet',
+            'my-3': payment != 'e-Wallet',
+          }"
+        >
+          {{ payment }}
+        </div>
+        <div
+          class="text-subtitle-1 font-weight-bold"
+          v-if="payment == 'e-Wallet'"
+        >
+          {{
+            toCurrency({
+              value: store.eWallet.balance,
+              locales: "en-US",
+              style: "decimal",
+            })
+          }}
+        </div>
+      </SelectionCard>
+    </v-col>
+  </v-row>
 </template>
